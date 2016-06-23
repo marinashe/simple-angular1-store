bde5a5819c8ae14b81ca1759bc21ac978dfb607e
Udi Oron <udioron@gmail.com>
2016-06-23 15:26:53 +0300

    Added ng-route and services.

--- store.js
+++ store.js
@@ -1,20 +1,63 @@
 "use strict";
 
-var m = angular.module("store", []);
+function indexBy(data, key='id') {
+    const d = {};
+    for (let p of data) {
+        d[p.id] = p;
+    }
+    return d;
+}
 
-m.controller('StoreCtrl', function StoreCtrl($http) {
+var m = angular.module("store", ["ngRoute"]);
+
+m.factory('inventory', function InventoryService($http) {
+    return $http.get('inventory.json').then(resp => resp.data);
+});
+
+m.factory('inventoryById', function InventorByIdService(inventory) {
+    return inventory.then(indexBy);
+});
+
+
+m.config(function ($routeProvider) {
+
+    //console.log('CONFIG PHASE', $routeProvider);
+
+    $routeProvider.when('/', {
+        templateUrl: 'inventory.html'
+    });
+
+    $routeProvider.when('/product/:id/', {
+        templateUrl: 'product.html',
+        controller: function ProductCtrl(product) {
+            this.product = product;
+        },
+        controllerAs: '$ctrl',
+        resolve: {
+            product: function ($route, inventoryById) {
+                return inventoryById.then(data => data[$route.current.params.id]);
+            }
+        }
+    });
+
+    $routeProvider.when('/foo/', {
+        templateUrl: 'foo.html'
+    });
+
+});
+
+
+m.controller('StoreCtrl', function StoreCtrl(inventory) {
     this.loaded = false;
     this.loadingMessage = "Loading inventory...";
-    $http.get('inventory.json').then(resp => {
-        this.inventory = resp.data;
+    inventory.then(data => {
+        this.inventory = data;
         this.loaded = true;
     }).catch(()=> {
         this.loadingMessage = "Something went terribly wrong. sorry."
     });
 
-    this.cartItems = [
-        // {product:this.inventory[0], amount: 1}
-    ];
+    this.cartItems = [];
 
     this.addToCart = product => {
         for (let p of this.cartItems) {
